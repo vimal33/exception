9812cfb82102c972f60954e8d3072fc6d638ea74
Vimal Raj .C <118258607+vimal33@users.noreply.github.com>
2022-12-05 21:55:50 +0530

    Add files via upload

---
+++ src/exception/Exception.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exception;
+
+/**
+ *
+ * @author Vimal
+ */
+public class Exception {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // TODO code application logic here
+        division vimal_divide = new division();
+        vimal_divide.divide();
+    }
+    
+    
+}
